b3329835cbf62697547d9005b7aa551ee5339e6f
Bansimplified567 <bansimplified567@gmail.com>
2025-07-05 20:21:53 +0800

    Updated

--- src/components/Pages/About/AboutSkills.jsx
+++ src/components/Pages/About/AboutSkills.jsx
@@ -9,248 +9,261 @@ import Sibonga from '../../img/logos/Sibonga.jpg';
 import './About.css';
 
 const badgeData = [
-   {
-      src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
-      alt: 'html5',
-   },
-   {
-      src: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
-      alt: 'css3',
-   },
-   {
-      src: 'https://img.shields.io/badge/javascript-%2314354C.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
-      alt: 'javascript',
-   },
-   {
-      src: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white',
-      alt: 'Sass',
-   },
-   {
-      src: 'https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white',
-      alt: 'json',
-   },
-   {
-      src: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white',
-      alt: 'tailwind',
-   },
-   {
-      src: 'https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E',
-      alt: 'vite',
-   },
-   {
-      src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
-      alt: 'react',
-   },
-   {
-      src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
-      alt: 'typescript',
-   },
-   {
-      src: 'https://img.shields.io/badge/PHP-777BB4.svg?style=for-the-badge&logo=php&logoColor=white',
-      alt: 'php',
-   },
-   {
-      src: 'https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white',
-      alt: 'cpp',
-   },
-   {
-      src: 'https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue',
-      alt: 'python',
-   },
-   {
-      src: 'https://img.shields.io/badge/prettier-1A2C34?style=for-the-badge&logo=prettier&logoColor=F7BA3E',
-      alt: 'prettier',
-   },
-   {
-      src: 'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white',
-      alt: 'github',
-   },
-   {
-      src: 'https://img.shields.io/badge/NPM-CA4245?style=for-the-badge&logo=npm&logoColor=white',
-      alt: 'npm',
-   },
-   {
-      src: 'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white',
-      alt: 'git',
-   },
-   {
-      src: 'https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white',
-      alt: 'vscode',
-   },
-   {
-      src: 'https://img.shields.io/badge/Open%20AI-0f9e7b.svg?style=for-the-badge&logo=openAI&logoColor=white',
-      alt: 'open-AI',
-   },
-   {
-      src: 'https://img.shields.io/badge/PowerShell-%235391FE.svg?style=for-the-badge&logo=powershell&logoColor=white',
-      alt: 'powershell',
-   },
-   {
-      src: 'https://img.shields.io/badge/Xampp-F37623?style=for-the-badge&logo=xampp&logoColor=white',
-      alt: 'xampp',
-   },
-   {
-      src: 'https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white',
-      alt: 'windows',
-   },
-   {
-      src: 'https://img.shields.io/badge/Debian-A81D33?style=for-the-badge&logo=debian&logoColor=white',
-      alt: 'debian',
-   },
-   {
-      src: 'https://img.shields.io/badge/Linux_Mint-87CF3E?style=for-the-badge&logo=linux-mint&logoColor=white',
-      alt: 'linux-mint',
-   },
-   {
-      src: 'https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white',
-      alt: 'ubuntu',
-   },
+  {
+    src: 'https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white',
+    alt: 'html5',
+  },
+  {
+    src: 'https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white',
+    alt: 'css3',
+  },
+  {
+    src: 'https://img.shields.io/badge/javascript-%2314354C.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E',
+    alt: 'javascript',
+  },
+  {
+    src: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white',
+    alt: 'Sass',
+  },
+  {
+    src: 'https://img.shields.io/badge/json-5E5C5C?style=for-the-badge&logo=json&logoColor=white',
+    alt: 'json',
+  },
+  {
+    src: 'https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white',
+    alt: 'tailwind',
+  },
+  {
+    src: 'https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E',
+    alt: 'vite',
+  },
+  {
+    src: 'https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB',
+    alt: 'react',
+  },
+  {
+    src: 'https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white',
+    alt: 'typescript',
+  },
+  {
+    src: 'https://img.shields.io/badge/PHP-777BB4.svg?style=for-the-badge&logo=php&logoColor=white',
+    alt: 'php',
+  },
+  {
+    src: 'https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white',
+    alt: 'cpp',
+  },
+  {
+    src: 'https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue',
+    alt: 'python',
+  },
+  {
+    src: 'https://img.shields.io/badge/prettier-1A2C34?style=for-the-badge&logo=prettier&logoColor=F7BA3E',
+    alt: 'prettier',
+  },
+  {
+    src: 'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white',
+    alt: 'github',
+  },
+  {
+    src: 'https://img.shields.io/badge/NPM-CA4245?style=for-the-badge&logo=npm&logoColor=white',
+    alt: 'npm',
+  },
+  {
+    src: 'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white',
+    alt: 'git',
+  },
+  {
+    src: 'https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white',
+    alt: 'vscode',
+  },
+  {
+    src: 'https://img.shields.io/badge/Open%20AI-0f9e7b.svg?style=for-the-badge&logo=openAI&logoColor=white',
+    alt: 'open-AI',
+  },
+  {
+    src: 'https://img.shields.io/badge/PowerShell-%235391FE.svg?style=for-the-badge&logo=powershell&logoColor=white',
+    alt: 'powershell',
+  },
+  {
+    src: 'https://img.shields.io/badge/Xampp-F37623?style=for-the-badge&logo=xampp&logoColor=white',
+    alt: 'xampp',
+  },
+  {
+    src: 'https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white',
+    alt: 'windows',
+  },
+  {
+    src: 'https://img.shields.io/badge/Debian-A81D33?style=for-the-badge&logo=debian&logoColor=white',
+    alt: 'debian',
+  },
+  {
+    src: 'https://img.shields.io/badge/Linux_Mint-87CF3E?style=for-the-badge&logo=linux-mint&logoColor=white',
+    alt: 'linux-mint',
+  },
+  {
+    src: 'https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white',
+    alt: 'ubuntu',
+  },
 ];
 const AboutSkills = () => {
-   return (
-      <section>
-         <motion.div
-            className="aboutTBody"
-            initial={{ opacity: 0, x: -100 }} // Slide in from the left
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5 }}
-         >
-            {badgeData.map((badge, index) => (
-               <div key={index}>
-                  <img src={badge.src} alt={badge.alt} height="35" width="100" />
-               </div>
-            ))}
-         </motion.div>
-         <p className="aboutSemiTitle">
-            - While I continue to focus on front-end technologies, I am dedicated to expanding my
-            backend skills to create more comprehensive and robust web applications. My goal is to
-            deliver exceptional user experiences by combining the best of both front-end and backend
-            development.
-         </p>
-      </section>
-   );
+  return (
+    <section>
+      <motion.div
+        className="aboutTBody"
+        initial={ { opacity: 0, x: -100 } } // Slide in from the left
+        whileInView={ { opacity: 1, x: 0 } }
+        transition={ { duration: 0.5 } }
+      >
+        { badgeData.map((badge, index) => (
+          <div key={ index }>
+            <img src={ badge.src } alt={ badge.alt } height="35" width="100" />
+          </div>
+        )) }
+      </motion.div>
+      <p className="aboutSemiTitle">
+        - While I continue to focus on front-end technologies, I am dedicated to expanding my
+        backend skills to create more comprehensive and robust web applications. My goal is to
+        deliver exceptional user experiences by combining the best of both front-end and backend
+        development.
+      </p>
+    </section>
+  );
 };
 
 const experiences = [
-   {
-      image: Rebisco,
-      alt: 'Rebisco',
-      role: 'Factory Worker \n (NOT-RELATED)',
-      company: 'Republic Biscuit Corporation (REBISCO)',
-      description:
-         'The Republic Biscuit Corporation, doing business as Rebisco and also known as the Rebisco Group of Companies, is a Philippine privately held multinational snack food company headquartered in Ortigas Center, Pasig.',
-      location: 'Bukidnon, Northern Mindanao, ',
-      duration: 'Nov 2021 - Apr 2022',
-      type: 'Full-time',
-      mode: 'On-site',
-   },
+  {
+    image: Rebisco,
+    alt: 'Rebisco',
+    role: 'Factory Worker \n (NOT-RELATED)',
+    company: 'Republic Biscuit Corporation (REBISCO)',
+    description:
+      'The Republic Biscuit Corporation, doing business as Rebisco and also known as the Rebisco Group of Companies, is a Philippine privately held multinational snack food company headquartered in Ortigas Center, Pasig.',
+    location: 'Bukidnon, Northern Mindanao, ',
+    duration: 'Nov 2021 - Apr 2022',
+    type: 'Full-time',
+    mode: 'On-site',
+  },
+  {
+    image: './banban.jpg', // Replace with actual image or import
+    alt: 'Freelance Web Developer',
+    role: 'Web Developer (Freelance)',
+    company: 'Self-Employed / Various Clients',
+    description:
+      'Worked as a freelance web developer delivering responsive, user-friendly websites and systems for small businesses and individual clients. Handled full-stack development tasks including front-end design, back-end integration, and database management. \n\nSkills used: JavaScript, HTML, CSS, React, Tailwind CSS, Bootstrap, PHP, MySQL.',
+    location: 'Remote / Cebu, Philippines',
+    duration: 'May 2023 - Present',
+    type: 'Freelance',
+    mode: 'Remote',
+  },
 ];
 
 const education = [
-   {
-      image: Lunocan,
-      alt: 'Lunocan',
-      role: 'Lunocan Elementary School',
-      description:
-         'Lunocan Elementary School, Cagayan de Oro - Bukidnon Hwy, Manolo Fortich, based on live traffic updates and road conditions',
-      location: 'Bukidnon, Northern Mindanao, Philippines',
-      duration: 'Apr 2015-2016',
-      type: 'Lunocan Manolo Fortich Bukidnon',
-      mode: 'Fulltime-Student',
-   },
-   {
-      image: Manolo,
-      alt: 'Manolo',
-      role: 'Manolo Fortich National Senior High School',
-      description:
-         'Manolo Fortich, Bukidnon, this school is one of the top-performing mega schools in the Division of Bukidnon',
-      location: 'Bukidnon, Northern Mindanao, Philippines',
-      duration: 'Apr 2021-2022',
-      type: 'Manolo Fortich Buk',
-      mode: 'Working-Student',
-   },
-   {
-      image: Sibonga,
-      alt: 'Sibonga',
-      role: 'Sibonga Community College',
-      description:
-         'Sibonga Community College is a private educational institution located in Cebu, Philippines. It is classified as a school and offers various programs and services to its students.',
-      location: 'Sibonga, Poblacion, Cebu, Philippines',
-      duration: 'August 2025-2026',
-      type: 'Sibonga Cebu',
-      mode: 'Working-Student',
-   },
+  {
+    image: Lunocan,
+    alt: 'Lunocan',
+    role: 'Lunocan Elementary School',
+    description:
+      'Lunocan Elementary School, Cagayan de Oro - Bukidnon Hwy, Manolo Fortich, based on live traffic updates and road conditions',
+    location: 'Bukidnon, Northern Mindanao, Philippines',
+    duration: 'Apr 2015-2016',
+    type: 'Lunocan Manolo Fortich Bukidnon',
+    mode: 'Fulltime-Student',
+  },
+  {
+    image: Manolo,
+    alt: 'Manolo',
+    role: 'Manolo Fortich National Senior High School',
+    description:
+      'Manolo Fortich, Bukidnon, this school is one of the top-performing mega schools in the Division of Bukidnon',
+    location: 'Bukidnon, Northern Mindanao, Philippines',
+    duration: 'Apr 2021-2022',
+    type: 'Manolo Fortich Buk',
+    mode: 'Working-Student',
+  },
+  {
+    image: Sibonga,
+    alt: 'Sibonga',
+    role: 'Sibonga Community College',
+    description:
+      'Sibonga Community College is a private educational institution located in Cebu, Philippines. It is classified as a school and offers various programs and services to its students.',
+    location: 'Sibonga, Poblacion, Cebu, Philippines',
+    duration: 'August 2025-2026',
+    type: 'Sibonga Cebu',
+    mode: 'Working-Student',
+  },
 ];
 
 const AboutExperienceArticle = ({
-   image,
-   alt,
-   role,
-   company,
-   description,
-   location,
-   duration,
-   type,
-   mode,
+  image,
+  alt,
+  role,
+  company,
+  description,
+  location,
+  duration,
+  type,
+  mode,
 }) => (
-   <article className="aboutExperienceArticle">
-      <div className="aboutExperienceRebisco">
-         <img src={image} alt={alt} className="aboutRebisco" />
-         <div className="aboutExperienceRebiscoDescription">
-            <h1 className="aboutParagraph">{role}</h1>
-            {company && <p>{company}</p>}
-            <p>{description}</p>
-            <p>{location}</p>
-         </div>
+  <article className="aboutExperienceArticle">
+    <div className="aboutExperienceRebisco">
+      <img src={ image } alt={ alt } className="aboutRebisco" />
+      <div className="aboutExperienceRebiscoDescription">
+        <h1 className="aboutParagraph">{ role }</h1>
+        { company && <p>{ company }</p> }
+        <p>{ description }</p>
+        <p>{ location }</p>
       </div>
-      <div className="aboutExperienceData">
-         <p>{duration}</p>
-         <p>{type}</p>
-         <p>{mode}</p>
-      </div>
-   </article>
+    </div>
+    <div className="aboutExperienceData">
+      <p>{ duration }</p>
+      <p>{ type }</p>
+      <p>{ mode }</p>
+    </div>
+  </article>
 );
 
 const AboutExperience = () => (
-   <>
-      <div>
-         <div className="aboutSection section">
-            <img src={flames} width="26px" alt="flames" />
-            <h1 className="aboutTitle">Experience</h1>
-            <img src={flames} width="26px" alt="flames" />
-         </div>
-         <motion.div
-            className="aboutSectionEducation"
-            initial={{ opacity: 0, x: 100 }} // Slide in from the right
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5 }}
-         >
-            {experiences.map((exp, index) => (
-               <AboutExperienceArticle key={index} {...exp} />
-            ))}
-         </motion.div>
+  <>
+    <div>
+      <div className="aboutSection section">
+        <img src={ flames } width="26px" alt="flames" />
+        <h1 className="aboutTitle">Experience</h1>
+        <img src={ flames } width="26px" alt="flames" />
       </div>
-      <img src={flamesBorder} className="flamesBorder" alt="flamesBorder" />
+      <motion.div
+        className="aboutSectionEducation"
+        initial={ { opacity: 0, x: 100 } } // Slide in from the right
+        whileInView={ { opacity: 1, x: 0 } }
+        transition={ { duration: 0.5 } }
+      >
+        { experiences.map((exp, index) => (
+          <AboutExperienceArticle key={ index } { ...exp } />
+        )) }
+      </motion.div>
+    </div>
+    <img src={ flamesBorder } className="flamesBorder" alt="flamesBorder" />
 
-      <div>
-         <div className="aboutSection">
-            <img src={flames} width="26px" alt="flames" />
-            <h1 className="aboutTitle">Education</h1>
-            <img src={flames} width="26px" alt="flames" />
-         </div>
-
-         <motion.div
-            className="aboutSectionEducation section"
-            initial={{ opacity: 0, x: 100 }} // Slide in from the right
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5 }}
-         >
-            {education.map((edu, index) => (
-               <AboutExperienceArticle key={index} {...edu} />
-            ))}
-         </motion.div>
+    <div>
+      <div className="aboutSection">
+        <img src={ flames } width="26px" alt="flames" />
+        <h1 className="aboutTitle">Education</h1>
+        <img src={ flames } width="26px" alt="flames" />
       </div>
-   </>
+
+      <motion.div
+        className="aboutSectionEducation section"
+        initial={ { opacity: 0, x: 100 } } // Slide in from the right
+        whileInView={ { opacity: 1, x: 0 } }
+        transition={ { duration: 0.5 } }
+      >
+        { education.map((edu, index) => (
+          <AboutExperienceArticle key={ index } { ...edu } />
+        )) }
+      </motion.div>
+    </div>
+  </>
 );
 
 export { AboutExperience, AboutSkills };
+
--- src/components/Pages/Extra/Footer/Footer.jsx
+++ src/components/Pages/Extra/Footer/Footer.jsx
@@ -12,92 +12,92 @@ import Messenger from '../../../img/logos/neon-twitter.png';
 import Youtube from '../../../img/logos/neon-youtube.png';
 
 function Footer() {
-   return (
-      <motion.section className="footer">
-         <div className="footerContainer">
-            <h1 className="footerTitle">BANBAN</h1>
-            <nav className="footernavbarContainer">
-               <FooterLink to="/about" title="Learn more about us" text="About" />
-               <FooterLink to="/blog" title="Read our latest posts" text="Blog" />
-               <FooterLink to="/contact" title="Get in touch with us" text="Contact" />
-               <FooterLink
-                  to="/activities"
-                  title="Find activities opportunities"
-                  text="Activities"
-               />
-               <FooterLink to="/portfolio" title="See our portfolio" text="Portfolio" />
-               <FooterLink to="/skills" title="What skills I acquired" text="Skills" />
-            </nav>
-         </div>
-         <img src={flamesBorder} className="flamesBorder" alt="flamesBorder" />
+  return (
+    <motion.section className="footer">
+      <div className="footerContainer">
+        <h1 className="footerTitle">BANBAN</h1>
+        <nav className="footernavbarContainer">
+          <FooterLink to="/about" title="Learn more about us" text="About" />
+          <FooterLink to="/blog" title="Read our latest posts" text="Blog" />
+          <FooterLink to="/contact" title="Get in touch with us" text="Contact" />
+          <FooterLink
+            to="/activities"
+            title="Find activities opportunities"
+            text="Activities"
+          />
+          <FooterLink to="/portfolio" title="See our portfolio" text="Portfolio" />
+          <FooterLink to="/skills" title="What skills I acquired" text="Skills" />
+        </nav>
+      </div>
+      <img src={ flamesBorder } className="flamesBorder" alt="flamesBorder" />
 
-         <div className="footerSection">
-            <div className="socialIcons">
-               <SocialLink
-                  url="https://www.linkedin.com/in/jade-ivan-bringcola-bb9466272/"
-                  imgSrc={Linkedin}
-                  alt="Linkedin"
-               />
-               <SocialLink
-                  url="https://www.instagram.com/nocodearea/"
-                  imgSrc={Instagram}
-                  alt="Instagram"
-               />
-               <SocialLink
-                  url="https://web.facebook.com/him.blacklion567/"
-                  imgSrc={Facebook}
-                  alt="Facebook"
-               />
-               <SocialLink url="https://x.com/JBringcola" imgSrc={Messenger} alt="Messenger" />
-               <SocialLink
-                  url="https://www.tiktok.com/@bansimplified?is_from_webapp=1&sender_device=pc"
-                  imgSrc={Tiktok}
-                  alt="Tiktok"
-               />
-               <SocialLink
-                  url="https://www.youtube.com/channel/UCHuVTj0sCJA5YQ_BUTcSPaw"
-                  imgSrc={Youtube}
-                  alt="Youtube"
-               />
-            </div>
+      <div className="footerSection">
+        <div className="socialIcons">
+          <SocialLink
+            url="https://www.linkedin.com/in/jade-ivan-bringcola-bb9466272/"
+            imgSrc={ Linkedin }
+            alt="Linkedin"
+          />
+          <SocialLink
+            url="https://www.instagram.com/nocodearea/"
+            imgSrc={ Instagram }
+            alt="Instagram"
+          />
+          <SocialLink
+            url="https://web.facebook.com/him.blacklion567/"
+            imgSrc={ Facebook }
+            alt="Facebook"
+          />
+          <SocialLink url="https://x.com/JBringcola" imgSrc={ Messenger } alt="Messenger" />
+          <SocialLink
+            url="https://www.tiktok.com/@bansimplified?is_from_webapp=1&sender_device=pc"
+            imgSrc={ Tiktok }
+            alt="Tiktok"
+          />
+          <SocialLink
+            url="https://www.youtube.com/channel/UCHuVTj0sCJA5YQ_BUTcSPaw"
+            imgSrc={ Youtube }
+            alt="Youtube"
+          />
+        </div>
 
-            <img
-               src="https://profile-counter.glitch.me/Blacklion567/count.svg"
-               alt="Profile Counter"
-            />
-            <div className="emailContainerInput">
-               <div className="inputWrapper">
-                  <input type="email" className="emailInput" placeholder="Enter your email" />
-                  <button type="submit">Subscribe</button>
-               </div>
-            </div>
-         </div>
-      </motion.section>
-   );
+        <img
+          src="https://profile-counter.glitch.me/Blacklion567/count.svg"
+          alt="Profile Counter"
+        />
+        <div className="emailContainerInput">
+          <div className="inputWrapper">
+            <input type="email" className="emailInput" placeholder="Enter your email" />
+            <button type="submit">Subscribe</button>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 }
 
 // FooterLink component for navigation links
 function FooterLink({ to, title, text }) {
-   const location = useLocation();
+  const location = useLocation();
 
-   const getLinkStyle = (path) => {
-      return location.pathname === path ? { color: '#FF3399' } : { color: '#fefefe' };
-   };
+  const getLinkStyle = (path) => {
+    return location.pathname === path ? { color: '#FF3399' } : { color: '#fefefe' };
+  };
 
-   return (
-      <Link className="navbarLinks" to={to} title={title} style={getLinkStyle(to)}>
-         {text}
-      </Link>
-   );
+  return (
+    <Link className="navbarLinks" to={ to } title={ title } style={ getLinkStyle(to) }>
+      { text }
+    </Link>
+  );
 }
 
 // SocialLink component for social media icons
 function SocialLink({ url, imgSrc, alt }) {
-   return (
-      <a href={url} target="_blank" rel="noopener noreferrer">
-         <img src={imgSrc} alt={alt} width="30px" />
-      </a>
-   );
+  return (
+    <a href={ url } target="_blank" rel="noopener noreferrer">
+      <img src={ imgSrc } alt={ alt } width="30px" />
+    </a>
+  );
 }
 
 export default Footer;
